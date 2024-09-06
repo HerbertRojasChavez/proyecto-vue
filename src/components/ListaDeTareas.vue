f3c9f1ffee922b9a9ecb860259c3722ac2728d73
Herbert Rojas <herbertmarcusserojaschavez1996@gmail.com>
2024-09-06 17:34:17 -0500

    Crea componenente Lista De tareas. Borra componetes autogenerados. Crea rutas

---
+++ src/components/ListaDeTareas.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h2>Lista de Tereas</h2>
+        <input type="text" v-model="nuevaTarea" @keyup.enter="addTarea">
+        <!-- <table>
+            <tr>
+                <td>categoria</td>
+                <td>cantidad</td>
+                <td>P/U</td>
+                <td>boton</td>
+            </tr>
+            <tr v-for="(tarea, index) in tareas" :key="index">
+                <td>{{ tarea }}</td>
+                <td><input type="text"></td>
+                <td><input type="text"></td>
+                <td><button @click="borrarTarea">X</button></td>
+            </tr>   
+        </table> -->
+        <ul>
+            <li v-for="(tarea, index) in tareas" :key="index">
+                {{ tarea }}
+                <button @click="borrarTarea">Borrar</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+    import { ref } from 'vue';
+    const tareas = ref([]);
+    const nuevaTarea = ref('');
+    const addTarea = () => {
+        tareas.value.push(nuevaTarea.value);
+        nuevaTarea.value = '';
+    }
+    const borrarTarea = (index) => {
+        tareas.value.splice(index,1);
+    }
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="conteiner">
+        <SideBar></SideBar>
+        <div class="content">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import SideBar from '../components/SideBar.vue';
+</script>
+
+<style>
+
+.conteiner {
+    display: flex;
+}
+.content {
+    flex: 1;
+    padding: 20px;
+}
+</style>
